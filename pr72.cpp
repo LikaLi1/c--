9f2998e51cf868568ecf74337f468d8d38f41ab8
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2025-12-11 14:13:27 +0300

    pr72.cpp

---
+++ pr72.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+void counter() {
+    static int count = 0;
+    count++;
+
+    std::cout << "Count: " << count << std::endl;
+}
+
+int main() {
+    counter();
+    counter();
+    counter();
+    counter();
+    counter();
+    counter();
+    counter();
+    counter();
+    counter();
+    counter();
+
+    return 0;
+}
