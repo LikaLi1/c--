5d655c01738b2a968ce496a9a08c26f7e3ff2eb6
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2025-12-18 14:13:18 +0300

    pr78.cpp

---
+++ pr78.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+void bubbleSortDescending(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] < arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int array[] = { 5, 2, 9, 1, 5, 6 };
+    int size = sizeof(array) / sizeof(array[0]);
+
+    bubbleSortDescending(array, size);
+
+    std::cout << "Array bubble: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
