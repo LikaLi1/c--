34a7a07f39b15eacef65d3980a5491833ebc7f58
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-01-13 10:45:12 +0300

    pr81.cpp

---
+++ pr81.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+void task_1() {
+    std::cout << "Enter first number: ";
+    int a;
+    std::cin >> a;
+    
+}
+
+void task_2() {
+   
+}
+
+void task_3() {
+    
+}
+
+void task_4() {
+    
+}
+
+void task_5() {
+    
+}
+
+void task_6() {
+    
+}
+
+void task_7() {
+    
+}
+
+void task_8() {
+
+}
+
+void task_9() {
+
+}
+
+void task_10() {
+
+}
+
+int main() {
+
+    task_1();
+    task_2();
+    task_3();
+    task_4();
+    task_5();
+    task_6();
+    task_7();
+    task_8();
+    task_9();
+    task_10();
+
+    return 0;
+
+}
