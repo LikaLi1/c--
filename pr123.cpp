fd55a7312a2741be5a9f0de6fb51a10586ccb97e
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-01-22 09:10:38 +0300

    pr123.cpp

---
+++ pr123.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A;
+    std::cout << "Enter number A: ";
+    std::cin >> A;
+
+    cout << "A * A = " << A * A << endl;
+
+    return 0;
+}
