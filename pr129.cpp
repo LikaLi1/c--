d0946bd35e3ea3a56cc5d273a8a81b9d5a5bb44a
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-01-26 19:30:19 +0300

    pr129.cpp

---
+++ pr129.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    const int size = 10;
+    int arr[size];
+    int sum = 0;
+
+    std::cout << "Enter array:\n";
+    for (int i = 0; i < size; i++)
+    {
+        std::cin >> arr[i];
+    }
+
+    std::cout << "Sum\n";
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 != 0) {
+            sum += arr[i];
+        }
+    }
+
+    std::cout << sum << std::endl;
+
+    return 0;
+}
