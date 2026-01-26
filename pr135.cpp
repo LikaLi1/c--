68d9d7d8739fdeac6ac3540d15e4f51acbf6aac3
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-01-26 21:21:02 +0300

    pr135.cpp

---
+++ pr135.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    const int size = 10;
+    int arr[size];
+
+    std::cout << "Enter array:\n";
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cin >> arr[i];
+    }
+
+    std::cout << "Min: \n";
+
+    int min = arr[0];
+    for (int i = 0; i < 5; ++i)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+        std::cout << min << std::endl;
+
+    return 0;
+}
