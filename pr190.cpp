ec9360aeab281161be4b3cd390968a2b2f2a8061
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-02-24 09:07:54 +0300

    pr190.cpp

---
+++ pr190.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void change(int* x) {
+    *x = 100;
+}
+
+int main()
+{
+    int a = 5;
+    change(&a);
+    std::cout << a;
+}
+
+#include <iostream>
+
+void change(int& x) {
+    x = 100;
+}
+
+int main()
+{
+    int a = 5;
+    change(a);
+    std::cout << a;
+}
